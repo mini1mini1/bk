6b63e006416377902b4b294421ac0648f4392b22
Byungkil Min <bkmins@gmail.com>
2018-08-11 15:32:48 +0900

    home20180811

--- secondServer/js/myScript.js
+++ secondServer/js/myScript.js
@@ -1,3 +1,21 @@
-function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+//var map = L.map('map').setView([37.504, 127.093], 14);
+
+var map = L.map('map').fitWorld();
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
 }
+
+map.on('locationfound', onLocationFound);
