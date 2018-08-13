a1a5f4bf53587001d87357d88c31eaf6f510cdd5
Byungkil Min <bkmins@gmail.com>
2018-08-13 10:57:41 +0900

    home0813

--- secondServer/index.js
+++ secondServer/index.js
@@ -1,7 +1,23 @@
+var fs = require("fs");
+var http = require
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var https = require('https');
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
+var credentials = {
+		  ca: fs.readFileSync('certs/maptok_com.ca-bundle'),
+		  key: fs.readFileSync('certs/maptok_com.key'),
+		  cert: fs.readFileSync('certs/maptok_com.crt'),
+		};
+
+
+var server= https.createServer(credentials, app);
+
+server.listen(app.get('8000'), function() {
+    console.log('Server up on port ' + app.get('8000'));
+});
 
 
 app.get('/', function(req, res){
@@ -14,7 +30,7 @@ app.get('/test', function(req, res){
 
 app.use('/', express.static(__dirname + '/'));
 
-io.on('connection', function(socket){
+/*io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(data){
 	console.log('from client:'+data.message);
@@ -24,4 +40,4 @@ io.on('connection', function(socket){
 
 http.listen(8000, function(){
   console.log('listening on *:8000');
-});
+});*/
