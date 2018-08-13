b1016a743d16ebc3f1c89736d2af40da6e352ad9
Byungkil Min <bkmins@gmail.com>
2018-08-13 10:58:37 +0900

    home0813_2

---
+++ secondServer/index2.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var http = require("http");
+var https = require('https');
+var express = require('express');
+var app = express();
+
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
+var credentials = {
+		  ca: fs.readFileSync('certs2/maptok_com.ca-bundle'),
+		  key: fs.readFileSync('certs2/maptok_com.key'),
+		  cert: fs.readFileSync('certs2/maptok_com.crt'),
+		};
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+app.get('/', function(req, res){
+	  res.sendFile(__dirname + '/w3.html');
+	});
+
+	app.get('/test', function(req, res){
+		  res.sendFile(__dirname + '/index.html');
+		});
+
+	app.use('/', express.static(__dirname + '/'));
+
+httpServer.listen(8080);
+httpsServer.listen(8000);
+
+/*io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(data){
+	console.log('from client:'+data.message);
+    io.emit('chat message', data);
+  });
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});*/
